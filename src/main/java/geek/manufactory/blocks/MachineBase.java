d7b6bdef682af082d3f96a8f1a8184cc69688c25
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-02 18:30:56 -0400

    added graph of machine values

--- src/main/java/geek/manufactory/blocks/MachineBase.java
+++ src/main/java/geek/manufactory/blocks/MachineBase.java
@@ -6,6 +6,15 @@ import net.minecraftforge.common.ToolType;
 
 public class MachineBase extends Block {
 	
+	/*
+	 * Mat.  Time  Fuel  F/t
+	 * Diam    75   150    2
+	 * Iron   100   300    3
+	 * Ston   150   600    4
+	 * Wood   300  1200    4
+	 * Gold    50  2400   48
+	 */
+	
 	private int speed;
 	private IItemTier toolTier;
 
