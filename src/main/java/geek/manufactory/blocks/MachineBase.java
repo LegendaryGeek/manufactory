bbb10650fa791db9e87a40c8bae94e3d0f904063
Jess Albertson <jess@albertson.dev>
2020-05-04 03:19:34 -0400

    further fleshed out machine base with base stats

--- src/main/java/geek/manufactory/blocks/MachineBase.java
+++ src/main/java/geek/manufactory/blocks/MachineBase.java
@@ -2,29 +2,40 @@ package geek.manufactory.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemTier;
 import net.minecraftforge.common.ToolType;
 
-public class MachineBase extends Block {
+public abstract class MachineBase extends Block {
 	
-	/*
-	 * Mat.  Time  Fuel  F/t
-	 * Diam    75   150    2
-	 * Iron   100   300    3
-	 * Ston   150   600    4
-	 * Wood   300  1200    4
-	 * Gold    50  2400   48
-	 */
-	
-	private int speed;
-	private IItemTier toolTier;
+	protected enum MaterialTier { // assuming it's internal to MachineBase
+		WOOD (1,1, ItemTier.WOOD),
+		STONE (2,2, ItemTier.STONE),
+		IRON (3,3, ItemTier.IRON),
+		DIAMOND (4,4, ItemTier.DIAMOND),
+		GOLD (6,0, ItemTier.GOLD);
+
+		final int speed;
+		final int fuelEfficiency;
+		final IItemTier toolTier;
+
+		MaterialTier (int speed, int fuelEfficiency, IItemTier toolTier) {
+			this.speed = speed;
+			this.fuelEfficiency = fuelEfficiency;
+			this.toolTier = toolTier;
+		}
+	}
+
+	// static variables used for internal calculations
+	private static final int FUEL_PER_BURN_TICK = 96;
+	private static final int TIME_PER_TICK = 12;
+
+
+	private MaterialTier tier;
 
 	public MachineBase(Properties properties) {
 		super(properties);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public MachineBase WoodTeir(MachineBase machine) {
-		return machine;
-	}
+
 
 }
