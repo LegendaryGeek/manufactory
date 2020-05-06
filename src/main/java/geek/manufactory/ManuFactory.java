6c7c43bb0e320aea58dbff2450c9fb3cc76fe6eb
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-05 23:14:09 -0400

    added registry event bus

--- src/main/java/geek/manufactory/ManuFactory.java
+++ src/main/java/geek/manufactory/ManuFactory.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -26,6 +27,7 @@ public class ManuFactory
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ManuFactory() {
+    	IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -34,6 +36,9 @@ public class ManuFactory
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        
+        Registry.BlocksRegistry.register(bus);
+        Registry.ItemsRegistry.register(bus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -71,14 +76,9 @@ public class ManuFactory
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
+
+    
+    public static String getModID() {
+    	return "manufactory";
     }
 }
