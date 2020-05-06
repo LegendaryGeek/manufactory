0bf7e21d91549d27cbe1583f56933feaf3d7f080
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-06 02:02:53 -0400

    access changes

--- src/main/java/geek/manufactory/ManuFactory.java
+++ src/main/java/geek/manufactory/ManuFactory.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("manufactory")
-public class ManuFactory
+public final class ManuFactory
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
--- src/main/java/geek/manufactory/Registry.java
+++ src/main/java/geek/manufactory/Registry.java
@@ -11,23 +11,23 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class Registry {
+public final class Registry {
 	 private static final Logger LOGGER = LogManager.getLogger();
 	
-	public static DeferredRegister<Block> BlocksRegistry = new DeferredRegister<>(ForgeRegistries.BLOCKS, ManuFactory.getModID());
-	public static DeferredRegister<Item> ItemsRegistry = new DeferredRegister<>(ForgeRegistries.ITEMS, ManuFactory.getModID());
+	protected  static DeferredRegister<Block> BlocksRegistry = new DeferredRegister<>(ForgeRegistries.BLOCKS, ManuFactory.getModID());
+	protected  static DeferredRegister<Item> ItemsRegistry = new DeferredRegister<>(ForgeRegistries.ITEMS, ManuFactory.getModID());
 
 	public static final RegistryObject<Block> tinBlock = registerBlock("tinBlock", new Block(Block.Properties.create(Material.IRON, MaterialColor.IRON)));
 	
 	public static final RegistryObject<Item> tinIngot = registerItem("tiningot", new Item(new Item.Properties()));
 	
-	public <T extends Block> RegistryObject<T> registerBlock(String name, T block) {
+	private static <T extends Block> RegistryObject<T> registerBlock(String name, T block) {
 		RegistryObject<T> register = BlocksRegistry.register(name, () -> block);
 		LOGGER.info("registered block: " + block.getRegistryName());
 		return register;
 	}
 	
-	public <T extends Item> RegistryObject<T> registerItem(String name, T item) {
+	private static <T extends Item> RegistryObject<T> registerItem(String name, T item) {
 		RegistryObject<T> register = ItemsRegistry.register(name, () -> item);
 		LOGGER.info("registered item: " + item.getRegistryName());
 		return register;
