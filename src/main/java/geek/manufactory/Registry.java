dcda03387b1a79b15a1d8c79513bbeec1571919d
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-06 01:25:44 -0400

    made the objects for registry static final

--- src/main/java/geek/manufactory/Registry.java
+++ src/main/java/geek/manufactory/Registry.java
@@ -17,9 +17,9 @@ public class Registry {
 	public static DeferredRegister<Block> BlocksRegistry = new DeferredRegister<>(ForgeRegistries.BLOCKS, ManuFactory.getModID());
 	public static DeferredRegister<Item> ItemsRegistry = new DeferredRegister<>(ForgeRegistries.ITEMS, ManuFactory.getModID());
 
-	public RegistryObject<Block> tinBlock = registerBlock("tinBlock", new Block(Block.Properties.create(Material.IRON, MaterialColor.IRON)));
+	public static final RegistryObject<Block> tinBlock = registerBlock("tinBlock", new Block(Block.Properties.create(Material.IRON, MaterialColor.IRON)));
 	
-	public RegistryObject<Item> tinIngot = registerItem("tiningot", new Item(new Item.Properties()));
+	public static final RegistryObject<Item> tinIngot = registerItem("tiningot", new Item(new Item.Properties()));
 	
 	public <T extends Block> RegistryObject<T> registerBlock(String name, T block) {
 		RegistryObject<T> register = BlocksRegistry.register(name, () -> block);
