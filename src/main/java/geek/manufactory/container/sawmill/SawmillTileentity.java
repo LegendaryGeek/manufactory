e0e80dab51cd51174376c50fb39ed5d0a720efca
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-10 15:59:11 -0400

    made a tile entity for the sawmill. still needs everything

---
+++ src/main/java/geek/manufactory/container/sawmill/SawmillTileentity.java
@@ -0,0 +1,26 @@
+package geek.manufactory.container.sawmill;
+
+import geek.manufactory.blocks.MachineBase;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.inventory.container.IContainerProvider;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.common.ForgeHooks;
+
+public class SawmillTileentity extends MachineBase {
+	
+	public static TileEntityType<SawmillTileentity> type;
+
+	public SawmillTileentity(MaterialTier tier) {
+		super(type, tier);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Container createMenu(int windowId, PlayerInventory inventory, PlayerEntity player) {
+		return new SawmillContainer(windowId, inventory);
+	}
+
+}
