ed49dc8cb1ae4e7b13b5d8a7570b5c402eb9528b
LegendaryGeek <45850917+LegendaryGeek@users.noreply.github.com>
2020-05-10 15:58:33 -0400

    Update AxeSlot.java

--- src/main/java/geek/manufactory/container/AxeSlot.java
+++ src/main/java/geek/manufactory/container/AxeSlot.java
@@ -1,6 +1,10 @@
 package geek.manufactory.container;
 
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTier;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
